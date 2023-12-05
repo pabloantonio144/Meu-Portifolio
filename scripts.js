1cf5403d4a0b338275da56e0e55d30f786f513b8
pabloantonio144 <pablo14@gmail.com>
2023-12-04 21:10:33 -0500

    atualizacao dos codigos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-    let header = document.querySelector('.header')
+
+let header = document.querySelector('#header')
 
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 200){
